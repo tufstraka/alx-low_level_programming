0b3f2133aa2afec83bf7ec85323c041adb37627a
kadimakeith <keithkadima@gmail.com>
2022-09-08 10:08:35 -0700

    first test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 1
+ */
+
+int main(void)
+{
+
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 50);
+	return (1);
+
+}
+
