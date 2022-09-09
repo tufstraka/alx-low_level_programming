9ec29aaac6973e072d72c8442078160dfc8421a8
kadimakeith <keithkadima@gmail.com>
2022-09-09 03:15:51 -0700

    Prints dynamic output based on the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <math.h>
 
 /**
  * main - entry point
@@ -12,31 +11,26 @@
 int main(void)
 {
 	int n;
+	int lastnum;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* Find last digit */
-	int lastDigit; 
-	lastDigit = n % 10;
+	lastnum = n % 10;
 
-	/* Total number of digits - 1 */
-	int digits = (int)log10(n);
 
-	/* Find first digit */
-	int firstDigit = n / pow(10, digits - 1);
-
-	if (lastDigit > 5)
+	if (lastnum > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastnum);
 	}
-	if (n == 0)
+	if (lastnum == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, firstDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastnum);
 	}
-	if (n < 6 && !0)
+	if (lastnum < 6 && !0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
 	}
 
 	return (0);
