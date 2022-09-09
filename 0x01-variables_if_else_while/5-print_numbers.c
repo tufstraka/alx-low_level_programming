0fbd8f96aed902354a6af5a5fdeb3ece1fec40dd
kadimakeith <keithkadima@gmail.com>
2022-09-09 08:19:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - prints numbers 0 to 9 followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while(x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+
+
+	return (0);
+}
