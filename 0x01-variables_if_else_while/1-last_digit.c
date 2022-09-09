4b1a6d4c54b514ff5f36659efa0bd67bc56eaf70
kadimakeith <keithkadima@gmail.com>
2022-09-09 03:08:56 -0700

    Prints dynamic output based on the last and first digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* Find last digit */
-	int lastDigit = n % 10;
+	int lastDigit; 
+	lastDigit = n % 10;
 
 	/* Total number of digits - 1 */
 	int digits = (int)log10(n);
