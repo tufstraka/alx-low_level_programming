1c82d5c73acea7e25e2f4894262ee242a18a5a9d
kadimakeith <keithkadima@gmail.com>
2022-09-12 04:41:13 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.c>
+
+/**
+ * add - finds the summation of the two arguments
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
