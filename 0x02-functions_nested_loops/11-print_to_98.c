d3497774df8ed9550f0e0e95a75b9ad53d0a697c
kadimakeith <keithkadima@gmail.com>
2022-09-12 04:51:32 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: parameter
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%i", i);
+		putchar(',');
+		putchar(' ');
+	}
+}
