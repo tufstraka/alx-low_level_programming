254c34e26ce3ddf1ad5d4e027bd71230ae74f68a
kadimakeith <keithkadima@gmail.com>
2022-09-14 00:47:15 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,13 +8,15 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	else
+	if (c >= 65 && c <= 90)
 	{
-		return (0);
+		return (1);
 	}
+
+	return (0);
 
 }
