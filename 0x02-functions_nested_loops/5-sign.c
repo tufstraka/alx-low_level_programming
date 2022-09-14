bcccfbeffb44e060396ffbd5d30b9f53633467ca
kadimakeith <keithkadima@gmail.com>
2022-09-14 00:50:31 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar("+");
 		return (1);
 	}
 	if (n == 0)
 	{
-		printf("0");
+		_putchar("0");
 		return (0);
 	}
 	if (n < 0)
 	{
-		printf("-");
+		_putchar("-");
 		return (-1);
 	}
 }
