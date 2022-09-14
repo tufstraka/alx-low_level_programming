4c4ba486d183a7c9332705c5e899bcd1f7128a6f
kadimakeith <keithkadima@gmail.com>
2022-09-14 00:33:20 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -9,6 +9,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar(95);
+	_putchar(122);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
 	return (0);
 }
