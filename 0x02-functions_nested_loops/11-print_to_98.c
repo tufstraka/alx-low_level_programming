4a84cbdbf9b2cc9681198dfd0f9eff16986f4a9f
kadimakeith <keithkadima@gmail.com>
2022-09-14 02:43:40 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,12 @@ void print_to_98(int n)
 	for (i = n; i <= 98; i++)
 	{
 		printf("%i", i);
-		putchar(',');
-		putchar(' ');
+		if (i == 98)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(',');
+		_putchar(' ');
 	}
 }
