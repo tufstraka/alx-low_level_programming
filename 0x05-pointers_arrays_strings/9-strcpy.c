cf15edeb7c8aaa9d31f05556c81e422a81ebfaff
Akashik Overlord <keithkadima@gmail.com>
2022-09-20 17:34:32 +0300

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
 /**
- * _strcpy - print reverse characters.
- * @dest: Array
- * @src: - Number of array
- * Return: Always 0.
+ * _strcpy -  copies the string pointed to by src, 
+ * including the terminating null byte (\0), 
+ * to the buffer pointed to by dest
+ *
+ * @dest: Target string memory address
+ *
+ * @src: Source string memory address
+ *
+ * Return: char.
  */
 char *_strcpy(char *dest, char *src)
 {
