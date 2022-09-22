b23436c98ec0f43ca38bb8a0660ad75ce2b78ed7
Akashik Overlord <keithkadima@gmail.com>
2022-09-22 17:13:03 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -24,7 +24,7 @@ char *_strncpy(char *dest, char *src, int n)
 		srclength++;
 	}
 
-	for (index = 0; index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 	{
 		/*Set the value at each index of src to its corresponding
 		 *index at dest until you reach the number of bytes specified
