8eb4ef9c018f45100838c0d70990f89131969f3b
Akashik Overlord <keithkadima@gmail.com>
2022-09-22 17:44:38 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: integer less than, equal to, or greater than zero if s1 is found,
+ *respectively, to be less than, to match, or be greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{ 
+  
