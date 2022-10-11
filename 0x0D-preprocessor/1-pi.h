30d3c0f33ca31d07ead8b93fe8bc3e0b1a2b34b2
kadimakeith <keithkadima@gmail.com>
2022-10-11 12:41:11 -0700

    Pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
