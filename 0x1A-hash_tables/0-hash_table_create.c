e1a4ca8621b6f4bac132ef9210004216ee2f36c1
tufstraka <keithkadima@gmail.com>
2023-12-11 09:30:48 +0300

    chore: optimize function
    combined memory allocation checks: Combined the checks for memory allocation of ht and ht->array to reduce redundancy.
    
    Cleanup on failure: Added a free(ht) statement to free the allocated memory for ht in case the allocation of ht->array fails. This ensures proper cleanup on failure.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,24 +4,27 @@
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * Return: A pointer to the new hash table, or NULL on failure.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+    hash_table_t *ht;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+    ht->size = size;
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL)
+    {
+        free(ht); // Cleanup on failure
+        return (NULL);
+    }
 
-	return (ht);
+    /* Initialize all elements of the array to NULL */
+    for (unsigned long int i = 0; i < size; i++)
+        ht->array[i] = NULL;
+
+    return (ht);
 }
